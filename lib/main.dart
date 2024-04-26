e6f9c1ce7d19edd076bd816bdea503ed25df450c
Kevin Tervoli <65734031+kevintervoli@users.noreply.github.com>
2024-04-26 00:30:12 -0400

    created the login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,49 +16,104 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Firestore Example'),
-        ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('users').snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            }
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                DocumentSnapshot document = snapshot.data!.docs[index];
-                return ListTile(
-                  title: Text(document['name']),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Surname: ${document['surname']}"),
-                      Text("Email: ${document['email']}"),
-                      Text("Username: ${document['username']}"),
-                      Text("Age: ${document['age']}"),  
-                      Text("Address: ${document['address']}"),
-                      Text("Password: ${document['password']}"),
-                    ],     
-        
-                  )
+      home: HomePage(),
+    );
+  }
+}
 
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({key});
 
-                );
-              },
-            );
-          },
-        ),
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // const Text("MedSync",
+          //     style: TextStyle(
+          //       color: Colors.black,
+          //       fontSize: 28.0,
+          //       fontWeight: FontWeight.bold,
+          //     )),
+          // const Text("Login to Your App",
+          //     style: TextStyle(
+          //       color: Colors.black,
+          //       fontSize: 48,
+          //       fontWeight: FontWeight.bold,
+          //     )),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top:25.0),
+              child: Image.asset(
+                'assets/medicine.png',
+                width: 100, // Adjust the width of the image
+                height: 100, // Adjust the height of the image
+                alignment:
+                    Alignment.center, // Centers the image within its container
+              ),
+            ),
+          ),
+          const SizedBox(height: 44.0),
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                hintText: "User Email",
+                prefixIcon: Icon(Icons.mail, color: Colors.black)),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: "User Password",
+                prefixIcon: Icon(Icons.lock, color: Colors.black)),
+          ),
+          const SizedBox(height: 12.0),
+          const Text("Forgot Password?", style: TextStyle(color: Colors.blue)),
+          const SizedBox(height: 88.0),
+          Container(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: Color(0xFF0069FE),
+              elevation: 0.0,
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)),
+              onPressed: () {},
+              child: Text('Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  )),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LoginScreen(),
     );
   }
 }
